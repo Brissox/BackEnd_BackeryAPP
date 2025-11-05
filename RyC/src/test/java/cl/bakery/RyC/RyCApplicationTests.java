8111b9504d912fca0e71158f2c4796c7d18cd354
Bastian Brisso <166576401+Brissox@users.noreply.github.com>
2025-11-05 15:19:59 +0000

    Se agregan carpetas de microservicios RyC y Soporte, los cuales solo tienen las carpetas iniciales

---
+++ RyC/src/test/java/cl/bakery/RyC/RyCApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bakery.RyC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
