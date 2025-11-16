c86125885a864279dd5142e25cbd1c0605c8c6da
Bastian Brisso <166576401+Brissox@users.noreply.github.com>
2025-11-16 03:47:03 +0000

    Se agrega security para el api.key ademas de la config de firebase

---
+++ Pagos/src/main/java/cl/bakery/Pagos/Config/FirebaseConfig.java
@@ -0,0 +1,31 @@
+package cl.bakery.Pagos.Config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initializeFirebase() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/bakeryweb-4145f-firebase-adminsdk-fbsvc-c453d06d6b.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+            System.out.println("Firebase Admin inicializado correctamente");
+        }
+    }
+}
+
+//INICIALIZACION DE FIREBASE PARA PODER USARLO EN EL PROYECTO
